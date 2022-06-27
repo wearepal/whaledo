22bc2cfd4226df00e8eed6ef9d05eb10e6157257
Myles Bartlett <m.bartlett@sussex.ac.uk>
2022-06-27 12:59:20 +0100

    Pass featur_dim to predictor.

--- whaledo/relay.py
+++ whaledo/relay.py
@@ -78,7 +78,7 @@ class WhaledoRelay(Relay):
         dm.setup()
 
         backbone, feature_dim = instantiate(self.backbone)()
-        predictor, out_dim = instantiate(self.predictor)()
+        predictor, out_dim = instantiate(self.predictor, _partial_=True)(feature_dim)
         model: Union[Model, MetaModel]
         model = Model(
             backbone=backbone,
